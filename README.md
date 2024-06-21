1fd844be4ba21ec70613530bb9c396449c11fbe6
Spencer Heywood <github@heywoodlh.io>
2024-06-21 13:46:01 -0600

    added nixos cloud-init image output

---
+++ README.md
@@ -0,0 +1,3 @@
+# Build the NixOS cloud-init image
+
+nix build .#image.x86_64-linux
--- flake.nix
+++ flake.nix
@@ -21,6 +21,79 @@
       terraform = pkgs.writeShellScriptBin "terraform" ''
         ${pkgs.opentofu}/bin/tofu $@
       '';
+
+      inherit (pkgs) lib;
+      arch = if pkgs.lib.hasInfix "aarch64" "${system}" then "aarch64" else "x86_64";
+      linuxSystem = "${arch}-linux";
+
+      baseModule = { lib, config, pkgs, ...}: {
+        nixpkgs.hostPlatform = "${linuxSystem}";
+        imports = [
+          "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+        ];
+
+        networking = {
+          hostName = "nixos-cloudinit";
+        };
+
+        fileSystems."/" = {
+          label = "nixos";
+          fsType = "ext4";
+          autoResize = true;
+        };
+        boot.loader.grub.device = "/dev/sda";
+        services.openssh.enable = true;
+        services.qemuGuest.enable = true;
+        security.sudo.wheelNeedsPassword = false;
+
+        users.users.heywoodlh = {
+          isNormalUser = true;
+          extraGroups = [ "wheel" ];
+        };
+
+        networking = {
+          dhcpcd.enable = true;
+          interfaces.eth0.useDHCP = false;
+        };
+
+        systemd.network.enable = true;
+
+        services.cloud-init = {
+          enable = true;
+          network.enable = true;
+          config = ''
+            system_info:
+              distro: nixos
+              network:
+                renderers: [ 'networkd' ]
+              default_user:
+                name: heywoodlh
+            users:
+                - default
+            ssh_pwauth: false
+            chpasswd:
+              expire: false
+            cloud_init_modules:
+              - migrator
+              - seed_random
+              - growpart
+              - resizefs
+            cloud_config_modules:
+              - disk_setup
+              - mounts
+              - set-passwords
+              - ssh
+            cloud_final_modules: []
+            '';
+        };
+      };
+
+      nixos = nixpkgs.lib.nixosSystem {
+        modules = [baseModule];
+      };
+
+      make-disk-image = import "${nixpkgs}/nixos/lib/make-disk-image.nix";
+
     in {
       devShell = pkgs.mkShell {
         name = "infrastructure";
@@ -34,6 +107,13 @@
         ];
       };
 
+      image = make-disk-image {
+        inherit pkgs lib;
+        config = nixos.config;
+        name = "nixos-cloudinit";
+        format = "qcow2-compressed";
+        copyChannel = false;
+      };
       formatter = pkgs.alejandra;
     });
 }
